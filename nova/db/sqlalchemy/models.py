60131083fc645517c8120c1d018b3ae79dcb5826
Belmiro Moreira <belmiro.moreira@cern.ch>
2015-09-16 13:58:42 +0200

    CERN - Add CERN database schema for network

--- nova/db/sqlalchemy/models.py
+++ nova/db/sqlalchemy/models.py
@@ -890,7 +890,20 @@ class FixedIp(BASE, NovaBase):
                                 'VirtualInterface.id,'
                                 'FixedIp.deleted == 0,'
                                 'VirtualInterface.deleted == 0)')
+# CERN
+    mac = Column(String(255), unique=True)
+    netcluster = Column(String(255))
+    address_v6 = Column(String(255))
+# CERN
 
+# CERN
+class CernNetwork(BASE, NovaBase):
+    """Represents an Ip Service configuration at CERN."""
+    __tablename__ = 'cern_network'
+    id = Column(Integer, primary_key=True)
+    netcluster = Column(String(255))
+    host = Column(String(255), unique=True)
+# CERN
 
 class FloatingIp(BASE, NovaBase):
     """Represents a floating ip that dynamically forwards to a fixed ip."""
