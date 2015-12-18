85e43f67b159479fbeac09d6ac8bbd3ce84f1e0d
Belmiro Moreira <belmiro.moreira@cern.ch>
2015-12-18 13:24:55 +0000

    CERN - Add scheduler live migration support per ipservice
    This excludes all hosts from different network services when
    performing a live migration without specifying a destination.

--- nova/conductor/tasks/live_migrate.py
+++ nova/conductor/tasks/live_migrate.py
@@ -23,6 +23,10 @@ from nova import objects
 from nova.scheduler import client as scheduler_client
 from nova.scheduler import utils as scheduler_utils
 from nova import servicegroup
+# CERN
+from nova import db
+from nova import context
+# CERN
 
 LOG = logging.getLogger(__name__)
 
@@ -155,11 +159,19 @@ class LiveMigrationTask(object):
                                                      self.instance)
         request_spec = scheduler_utils.build_request_spec(self.context, image,
                                                           [self.instance])
-
+# CERN
+        ctxt = context.get_admin_context()
+        ipservice = db.cern_netcluster_get(ctxt, self.instance['host'])
+        ignore_hosts = db.cern_ignore_hosts(ctxt, ipservice['netcluster'])
+# CERN
         host = None
         while host is None:
             self._check_not_over_max_retries(attempted_hosts)
-            filter_properties = {'ignore_hosts': attempted_hosts}
+# CERN
+            filter_properties = {'ignore_hosts': ignore_hosts}
+            filter_properties['ignore_hosts'].extend(attempted_hosts)
+            LOG.info("Live migration skipping hosts: %s" % filter_properties['ignore_hosts'])
+# CERN
             scheduler_utils.setup_instance_group(self.context, request_spec,
                                                  filter_properties)
             host = self.scheduler_client.select_destinations(self.context,
