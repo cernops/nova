0e68f16c8873b37b7e6483a2546a653c507590bf
Belmiro Moreira <belmiro.moreira@cern.ch>
2016-04-05 16:00:21 +0200

    CERN - Skip EC2 VMs from waiting for DNS
    The new EC2 service introduced a new naming for VMs.
    Those don't wait for DNS update.
    
    nova/compute/manager.py# with '#' will be ignored, and an empty message aborts the commit.

--- nova/compute/manager.py
+++ nova/compute/manager.py
@@ -1430,6 +1430,9 @@ class ComputeManager(manager.Manager):
         if instance['hostname'] == "server-"+str(instance['uuid']):
             return
 
+        if instance['hostname'].startswith('server-r-'):
+            return
+
         instance_hostname = str(instance['hostname'])
         meta = utils.instance_meta(instance)
 
